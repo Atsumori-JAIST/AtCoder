b57fb007e168f82b055b6838b9dc2487c2f11e9c
Atsumori-JAIST <s2010401@jaist.ac.jp>
2021-05-25 19:49:33 +0900

    In the middle

--- 081b.py
+++ 081b.py
@@ -1,9 +1,28 @@
-n = 0               # 要素数 N
-input_num = []      # 入力数値の配列
+def main():
+    n = 0               # 要素数 N
+    input_num = []      # 入力数値の配列
 
-# 入力データ格納
-n = input()
-input_num = list(map(int, input().split()))
+    # 入力データ格納
+    n = input()
+    input_num = list(map(int, input().split()))
 
-print(n)
-print(input_num)
+    # 要素数チェック
+    valid(n, input_num)
+
+    # メイン処理
+    
+
+
+# 要素数チェック
+def valid(num, in_list):
+    if num != len(in_list):
+        exit()
+
+# 2で割る
+def div_2():
+    # ToDo
+    print("作成途中")
+
+
+if __name__ == "__main__":
+    main()

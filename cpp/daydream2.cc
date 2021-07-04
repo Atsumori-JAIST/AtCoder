af44c56e2e312b83fa79bda4476237acd36bbcd5
atsumori <s2010401@jaist.ac.jp>
2021-07-04 12:05:45 +0900

    Modified Daydream2

--- cpp/daydream2.cc
+++ cpp/daydream2.cc
@@ -15,7 +15,6 @@ int main() {
 
   while (input.empty() != true) {
 
-    /*
     auto tmp = find(chars.begin(), chars.end(), input[idx]);
     if (tmp == chars.end()) {
       cout << "NO" << endl;
@@ -23,48 +22,54 @@ int main() {
     } else {
       chunk += input[idx];
     }
-    */
-    chunk += input[idx];
 
-    if (idx == 7) {
-
-      cout << chunk << endl;
-      
-      if (chunk.erase(6) == "eraser") {
-        input.erase(input.begin(), input.begin()+6);
-        chunk.clear();
-        idx = 0;
-      } else if (chunk.erase(5) == "erase") {
-        input.erase(input.begin(), input.begin()+5);
-        chunk.clear();
-        idx = 0;
-        cout << input << endl;
-        cout << chunk.empty() << endl;
-      } else if (chunk.erase(7) == "dreamer") {
-        if (chunk.rfind("a") != 7) {
-          input.erase(input.begin(), input.begin()+7);
-          chunk.clear();
-          idx = 0;
-        } else {
-          input.erase(input.begin(), input.begin()+5);
-          chunk.clear();
-          idx = 0;
+    switch (input.length()) {
+      case 5:
+        if (input.substr(0,5)=="erase" || input.substr(0,5)=="dream") {
+          cout << "YES" << endl;
+          goto OUT;
         }
-      } else {
-        cout << "Not Match!!" << endl;
-        break;
-      }
-
-      if (!chunk.empty()) {
-        cout << "NO" << endl;
-        break;
-      }
+      case 6:
+        if (input.substr(0.6) == "eraser") {
+          cout << "YES" << endl;
+          goto OUT;
+        }
+      case 7:
+        if (input.substr(0,7) == "dreamer") {
+          cout << "YES" << endl;
+          goto OUT;
+        }
+      default:
+        if (idx == 7) {
+      
+          if (chunk.substr(0,6) == "eraser") {
+            input.erase(input.begin(), input.begin()+6);
+            chunk.clear();
+            idx = 0;
+          } else if (chunk.substr(0,5) == "erase") {
+            input.erase(input.begin(), input.begin()+5);
+            chunk.clear();
+            idx = 0;
+          } else if (chunk.substr(0,7) == "dreamer") {
+            if (chunk.rfind("a") != 7) {
+              input.erase(input.begin(), input.begin()+7);
+              chunk.clear();
+              idx = 0;
+            } else {
+              input.erase(input.begin(), input.begin()+5);
+              chunk.clear();
+              idx = 0;
+            }
+          } else {
+            cout << "NO" << endl;
+            goto OUT;
+          }
+        }
     }
-
 
     idx++;
   }
 
-  cout << "DEBUG!!" << endl;
+OUT:
   return 0;
 }
